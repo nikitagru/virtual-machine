7caa7b430445e0337ce5a06f28ecb4520da5dafe
nikitagru <50076302+nikitagru@users.noreply.github.com>
2020-03-16 00:28:16 +0500

    final

--- virtual machine/script.js
+++ virtual machine/script.js
@@ -1,6 +1,6 @@
 const FS = require('fs');
 const readline = require('readline-sync');
-const COMMANDS = ["bekommen", "legen", "wenn", "senken", "mul", "gleich", "uber", "@"];
+const COMMANDS = ["bekommen", "legen", "wenn", "senken", "mul", "gleich", "uber", "@", "geben"];
 
 let prog = FS.readFileSync('main.txt').toString().split("\r\n");
 
@@ -10,8 +10,8 @@ let memory = [];
 let i = 0;
 
 
-for (let item of prog) {
-    let command = item.slice(0, item.indexOf(' '));                                     // command of program; first word
+for (let i = 0; i < prog.length; i++) {
+    let command = prog[i].slice(0, prog[i].indexOf(' '));                                     // command of program; first word
     if (command[0] == "@") {
         command = command[0];
     }
@@ -21,7 +21,7 @@ for (let item of prog) {
     switch (command) {
         case COMMANDS[0]:
             function bekommen() {
-                memoryPoint = Number(item.slice(item.indexOf(' ') + 1, command.length + 5));    // pointer to memory adress
+                memoryPoint = Number(prog[i].slice(prog[i].indexOf(' ') + 1, command.length + 5));    // pointer to memory adress
                 if (memory[memoryPoint % 1000] == undefined) {
                     let temp = Number(readline.question('Enter the value'));                // get a value from user
                     
@@ -39,9 +39,9 @@ for (let item of prog) {
 
         case COMMANDS[1]:
             function legen() {
-                memoryPoint = Number(item.slice(item.indexOf(' ') + 1, command.length + 5));    // pointer to memory adress
+                memoryPoint = Number(prog[i].slice(prog[i].indexOf(' ') + 1, command.length + 5));    // pointer to memory adress
                 if (memory[memoryPoint % 1000] == undefined) {
-                    let value = item.slice(currentPos, item.length);                        // value, which programm have to set in memory
+                    let value = prog[i].slice(currentPos, prog[i].length);                        // value, which programm have to set in memory
                     
                     if (value[0] == '_') {                                                  // if the value is memoryPoint
                         let tempMemoryPoint = Number(value.slice(1));                       // get memoryPoint of value, which programm have to set in memoryPoint
@@ -58,15 +58,15 @@ for (let item of prog) {
 
         case COMMANDS[2]:
             function wenn() {
-                memoryPoint = item.slice(item.indexOf(' ') + 1, command.length + 6);    // pointer to memory adress
-                let space = item.indexOf(' ', currentPos + 1);
-                let operator = item.slice(currentPos + 1, space);
+                memoryPoint = prog[i].slice(prog[i].indexOf(' ') + 1, command.length + 6);    // pointer to memory adress
+                let space = prog[i].indexOf(' ', currentPos + 1);
+                let operator = prog[i].slice(currentPos + 1, space);
                 
                 if (memoryPoint[0] == '_') {
                     memoryPoint = Number(memoryPoint.slice(1));
                 }
                 
-                let secondValue = item.slice(currentPos + operator.length + 2, item.indexOf(' ', currentPos + operator.length + 2));
+                let secondValue = prog[i].slice(currentPos + operator.length + 2, prog[i].indexOf(' ', currentPos + operator.length + 2));
                 if (secondValue[0] == '_') {
                     secondValue = Number(secondValue.slice(1));
                 }
@@ -74,8 +74,8 @@ for (let item of prog) {
                 if (operator == COMMANDS[5]) {
                 
                     if (memory[memoryPoint % 1000] == memory[secondValue % 1000]) {
-                        let condCodeStart = item.indexOf('=>');
-                        let codeBlock = item.slice(condCodeStart + 3, item.length);
+                        let condCodeStart = prog[i].indexOf('=>');
+                        let codeBlock = prog[i].slice(condCodeStart + 3, prog[i].length);
                         
                         if (codeBlock.slice(0, 5) == 'geben') {
                             let value = codeBlock.slice(5, codeBlock.length);
@@ -89,11 +89,14 @@ for (let item of prog) {
                     }
                 } else if (operator == COMMANDS[6]) {
                     if (memory[memoryPoint % 1000] > memory[secondValue % 1000]) {
-                        let condCodeStart = item.indexOf('=>');
-                        let codeBlock = item.slice(condCodeStart + 3, item.length);
+                        let condCodeStart = prog[i].indexOf('=>');
+                        let codeBlock = prog[i].slice(condCodeStart + 3, prog[i].length);
 
                         if (codeBlock[0] == "@") {
-                            myFunction();
+                            let stopIndex = i;
+                            if(nameOfFunction != undefined) {
+                                i = indexOfFunction;
+                            }
                         }
                     }
                 }
@@ -101,23 +104,72 @@ for (let item of prog) {
             wenn();
             break;
         
-        case COMMANDS[4]:
+        case COMMANDS[3]:
             function senken() {
-                memoryPoint = Number(item.slice(item.indexOf(' ') + 1, command.length + 5));
+                memoryPoint = Number(prog[i].slice(prog[i].indexOf(' ') + 1, command.length + 5));
                 memory[memoryPoint % 1000]--;
             }
             senken();
             break;
         
         case COMMANDS[4]:
+            function mul() {
 
+                memoryPoint = Number(prog[i].slice(prog[i].indexOf(' ') + 1, command.length + 5));
+                
+                let space = prog[i].indexOf(' ', currentPos + 1);
+                let firstValue = prog[i].slice(currentPos, space);
+                
+                currentPos += firstValue.length;
+                let secondValue = prog[i].slice(currentPos + 1, prog[i].length);
+
+                
+                if (isNaN(firstValue) && isNaN(secondValue)) {
+                    if (firstValue[0] == '_') {
+                        firstValue = Number(firstValue.slice(1));
+
+                    }
+                    if (secondValue[0] == '_') {
+                        secondValue = Number(secondValue.slice(1));
+
+                    }
+                    memory[memoryPoint % 1000] = memory[firstValue % 1000] * memory[secondValue % 1000];
+                    // console.log(memory);
+                } else if (isNaN(firstValue) && !isNaN(secondValue)) {
+                    if (firstValue[0] == '_') {
+                        firstValue = Number(firstValue.slice(1));
+                    }
+                    memory[memoryPoint % 1000] = memory[firstValue % 1000] * memory[secondValue % 1000];
+                    // console.log(memory);
+                } else if (!isNaN(firstValue) && isNaN(secondValue)) {
+                    if (secondValue[0] == '_') {
+                        secondValue = Number(secondValue.slice(1));
+                    }
+                    memory[memoryPoint % 1000] = memory[firstValue % 1000] * memory[secondValue % 1000];
+                    // console.log(memory);
+                }
+
+            }
+            mul();
             break;
 
         case COMMANDS[7]:
-            
+            var nameOfFunction = prog[i].slice(1, prog[i].length);
+            var indexOfFunction = i;
+            break;
 
+        case COMMANDS[8]:
+            memoryPoint = prog[i].slice(prog[i].indexOf(' ') + 1, command.length + 6);
+            console.log(memoryPoint);
+            if (isNaN(memoryPoint)) {
+                if(memoryPoint[0] == '_') {
+                    memoryPoint = Number(memoryPoint.slice(1));
+                    console.log(memory[memoryPoint % 1000]);
+                }
+            }
             break;
     }
 
     
 }
+
